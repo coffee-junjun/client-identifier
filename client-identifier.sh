ce88e237a6fc116ed9362a96c639b0ff23462881
coffeejunjun <coffeesugarsi5@gmail.com>
2020-05-24 11:45:46 +0900

    [add] 引数がない場合、多い場合の処理を追加

--- client-identifier.sh
+++ client-identifier.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
-MAC=${1}
-MAC=`echo "$MAC" | sed s/[:-]//g`
-MAC=01$MAC
-MAC=`echo "$MAC" | sed -e 's/\(....\)/\1./g'`
-echo $MAC
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "引数がありません。"
+  exit 1
+elif [ $# -ge 2 ]; then
+  echo "引数が1つではありません。"
+  exit 1
+else
+  MAC=${1}
+  MAC=`echo "$MAC" | sed s/[:-]//g`
+  MAC=01$MAC
+  MAC=`echo "$MAC" | sed -e 's/\(....\)/\1./g'`
+  echo $MAC
+fi
